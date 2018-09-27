7865cb36841e20dd3936c768bd491da4b0721e36
Maciej Zduniak <mzduniak@hotmail.com>
2018-09-28 00:31:04 +0200

    mod

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace WpfApp1
 {
+
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
@@ -23,6 +14,31 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            string fileName = null;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    fileName = openFileDialog1.FileName;
+            //}
+            
+
+            if (fileName != null)
+            {
+                //Do something with the file, for example read text from it
+                string text = File.ReadAllText(fileName);
+            }
         }
 
     }
